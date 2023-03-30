5e0ce790854022d34ec4579833986035cd522823
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2023-03-30 19:21:47 +0300

    Добавьте файлы проекта.

---
+++ GitTest1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace GitTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
